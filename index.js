50c73d087eee4b22d176f239dbdc16bde2179939
Chris <sideresponse@protonmail.com>
2021-09-02 19:52:35 -0500

    First commit

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-const express = require('express');
-const app = express();
+import express from 'express';
+import {getAppContext} from './context/AppContext.js';
+
+const appContext = getAppContext(process.env.NODE_ENV);
+console.log('appcontext: ', appContext);
+//const app = express();
 
 // Please use apollo server to implement your graphql query
 // const { ApolloServer } = require('apollo-server-express');
@@ -9,10 +13,10 @@ const app = express();
 // server.applyMiddleware({ app, path:"/graphql" });
 
 /** Please remove me line 11-14 **/
-app.get('*', (req, res, next) => {
-    res.send("Good luck! 😀")
-});
+// app.get('*', (req, res, next) => {
+//     res.send("Good luck! 😀")
+// });
 
-app.listen({ port: 4000 }, () =>
-    console.log(`Listening on http://localhost:4000/graphql`)
-);
+// app.listen({ port: 4000 }, () =>
+//     console.log(`Listening on http://localhost:4000/graphql`)
+// );
